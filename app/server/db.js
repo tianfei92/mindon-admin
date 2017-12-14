5b728ff992a11cbc1f76646fc5eaabec92eecbc8
arielta <arieltian1992@gmail.com>
2017-12-14 16:22:15 +0800

    链接数据库

--- app/server/db.js
+++ app/server/db.js
@@ -1,10 +1,10 @@
 // 数据库连接配置
 module.exports = {
     mysql: {
-        host: 'localhost',
+        host: '127.0.0.1',
         user: 'root',
-        password: 'Qiuqiu@0903',
-        database: 'test',
+        password: 'arieltian@1992',
+        database: 'sys',
         port: '3306'
     }
 }
