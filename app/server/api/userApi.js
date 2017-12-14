92892550e46d6d376a6a3ad500305d355b3375ad
tianfeib <tianfeib@zbj.com>
2017-12-14 16:22:15 +0800

    链接数据库

--- app/server/api/userApi.js
+++ app/server/api/userApi.js
@@ -5,19 +5,19 @@ var mysql = require('mysql');
 var $sql = require('../sqlMap');
 
 // 连接数据库
-// var conn = mysql.createConnection(models.mysql);
-//
-// conn.connect();
-// var jsonWrite = function(res, ret) {
-//     if(typeof ret === 'undefined') {
-//         res.json({
-//             code: '1',
-//             msg: '操作失败'
-//         });
-//     } else {
-//         res.json(ret);
-//     }
-// };
+var conn = mysql.createConnection(models.mysql);
+
+conn.connect();
+var jsonWrite = function(res, ret) {
+    if(typeof ret === 'undefined') {
+        res.json({
+            code: '1',
+            msg: '操作失败'
+        });
+    } else {
+        res.json(ret);
+    }
+};
 
 // 增加用户接口
 router.post('/addUser', (req, res) => {
