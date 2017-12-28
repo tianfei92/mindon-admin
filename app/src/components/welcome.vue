64de5bcf1a64e11c45a7a2a242b43076556ec080
tianfeib <tianfeib@zbj.com>
2017-12-28 23:15:01 +0800

    子组件通知父组件

--- app/src/components/welcome.vue
+++ app/src/components/welcome.vue
@@ -1,4 +1,5 @@
 <template>
+    <!--接受父组件传来的消息-->
     <h1>{{ message }}</h1>
 </template>
 
@@ -7,7 +8,7 @@
         name: "welcome",
         props: ['message'],
         created() {
-            this.$emit('contact')
+            this.$emit('contact'); //通知父组件
         }
     }
 </script>
