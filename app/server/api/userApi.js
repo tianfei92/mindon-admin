0d4d584eebda5e8f1cecf0d7b2fa7071a17dcefb
tianfeib <tianfeib@zbj.com>
2018-01-01 22:04:41 +0800

    登录实现

--- app/server/api/userApi.js
+++ app/server/api/userApi.js
@@ -25,16 +25,29 @@ var jsonWrite = function (res, ret) {
 };
 
 // 增加用户接口
-router.post('/addUser', (req, res) => {
-    let sql = $sql.user.add;
+router.post('/login', (req, res) => {
+    // let sql = $sql.user.add;
     let params = req.body;
+    let sql = `select count(1) from user_log_in where name='${params.username}' and password='${params.password}'`;
 
-    conn.query(sql, [params.username, params.age], function (err, result) {
+    conn.query(sql, [params.username, params.password], function (err, result) {
         if (err) {
-            console.log(err);
+            console.log(`${err}*************系统错误*************`);
+            const resData = {
+                code: 1001,
+                success: false,
+                msg: '系统错误'
+            };
+            res.send(resData)
         }
         if (result) {
-            jsonWrite(res, result);
+            // jsonWrite(res, result);
+            const resData = {
+                code: 0,
+                success: true,
+                msg: '登录成功'
+            };
+            res.send(resData)
         }
     })
 });
