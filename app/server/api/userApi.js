f9e819d36c2db8510b80e380bae01fdfed96e325
tianfeib <tianfeib@zbj.com>
2018-01-02 22:54:17 +0800

    update

--- app/server/api/userApi.js
+++ app/server/api/userApi.js
@@ -1,28 +1,13 @@
-var models = require('../db');
-var express = require('express');
-var router = express.Router();
-var mysql = require('mysql');
-var $sql = require('../sqlMap');
+const models = require('../db');
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+const $sql = require('../sqlMap');
 
 // 连接数据库
-var conn = mysql.createConnection(models.mysql);
+let conn = mysql.createConnection(models.mysql);
 
 conn.connect();
-var jsonWrite = function (res, ret) {
-    if (typeof ret === 'undefined') {
-        res.json({
-            code: 1,
-            success: false,
-            msg: '操作失败'
-        });
-    } else {
-        res.json({
-            code: 0,
-            success: true,
-            data: ret
-        });
-    }
-};
 
 // 增加用户接口
 router.post('/login', (req, res) => {
@@ -30,26 +15,30 @@ router.post('/login', (req, res) => {
     let params = req.body;
     let sql = `select count(1) from user_log_in where name='${params.username}' and password='${params.password}'`;
 
-    conn.query(sql, [params.username, params.password], function (err, result) {
-        if (err) {
-            console.log(`${err}*************系统错误*************`);
-            const resData = {
-                code: 1001,
-                success: false,
-                msg: '系统错误'
-            };
-            res.send(resData)
-        }
-        if (result) {
-            // jsonWrite(res, result);
-            const resData = {
-                code: 0,
-                success: true,
-                msg: '登录成功'
-            };
-            res.send(resData)
-        }
-    })
+    try {
+        conn.query(sql, [params.username, params.password], function (err, result) {
+            if (err) {
+                console.log(`${err}*************系统错误*************`);
+                const resData = {
+                    code: 1001,
+                    success: false,
+                    msg: '系统错误'
+                };
+                res.send(resData)
+            }
+            if (result) {
+                const resData = {
+                    code: 0,
+                    success: true,
+                    msg: '登录成功'
+                };
+                res.send(resData);
+            }
+        })
+    }
+    catch (e){
+        console.log('*****************系统错误*****************')
+    }
 });
 
 module.exports = router;
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -12,6 +12,11 @@ export default new Router({
             path: '/',
             name: 'HelloWorld',
             component: HelloWorld
+        },
+        {
+            path: '/index',
+            name: 'HelloWorld',
+            component: HelloWorld
         }
     ]
 })
