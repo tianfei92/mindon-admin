6b7190f01cd292bf3e3f961570fadfded8126972
tianfeib <tianfeib@zbj.com>
2018-01-04 00:22:25 +0800

    update

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Resource from 'vue-resource'
 import HelloWorld from '../components/HelloWorld'
+import promise from '../components/promise'
 
 Vue.use(Router)
 Vue.use(Resource)
@@ -12,6 +13,11 @@ export default new Router({
             path: '/',
             name: 'HelloWorld',
             component: HelloWorld
+        },
+        {
+            path: '/promise',
+            name: 'promise',
+            component: promise
         }
     ]
 })
